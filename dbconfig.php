bb8c7f1a1052e2cf9d9db14c384004c97060ed92
ysec <80327113+yezins@users.noreply.github.com>
2023-09-11 23:04:16 +0800

    Create dbconfig.php

---
+++ dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+	!defined('DB_HOST') && define('DB_HOST','101.43.64.111');
+	!defined('DB_USER') && define('DB_USER','root');
+	!defined('DB_PASS') && define('DB_PASS','root123');
+	!defined('DB_NAME') && define('DB_NAME','QHCY');
+	!defined('DB_CHARSET') && define('DB_CHARSET','utf8');
+$sex=array('保密','男','女');
+$edu=array('保密','小学','初中','高中/中专','大专','本科','研究生','博士','博士后');
+$admins=array('普通用户','管理员')
+?>
