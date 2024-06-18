668fa24f9f33811916c5c98f214b3fc94776a0cb
ysec <80327113+yezins@users.noreply.github.com>
2024-06-18 15:38:09 +0800

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -5,6 +5,6 @@
 	!defined('DB_NAME') && define('DB_NAME','QHCY');
 	!defined('DB_CHARSET') && define('DB_CHARSET','utf8');
 $sex=array('保密','男','女');
-$edu=array('保密','小学','初中','高中/中专','大专','本科','研究生','博士','博士后','柴达木学院');
+$edu=array('保密','小学','初中','高中/中专','大专','本科','研究生','博士','博士后','德令哈');
 $admins=array('普通用户','管理员')
 ?>
